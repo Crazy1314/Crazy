fdcb6f6474703f2c2bff66db5b38e01b61cc6823
gaoyufeng <253114555@qq.com>
2016-06-15 15:23:37 +0800

    jquery-1.8.3.min.js

--- xiangmu/xiangmu_admin/application/config/database.php
+++ xiangmu/xiangmu_admin/application/config/database.php
@@ -75,22 +75,16 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '192.168.1.126',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'da_hufupin',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+	'char_set' => 'utf8'
+	
 );
