b3ec75297f929bdd1a632b2d66b8677fc7725ffb
bai <1253475445@qq.com>
2016-06-16 11:16:16 +0800

    database

--- xiangmu/xiangmu_admin/application/config/database.php
+++ xiangmu/xiangmu_admin/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '192.168.1.126',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'da_hufupin',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
