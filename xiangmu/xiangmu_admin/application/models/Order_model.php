c679efc0fc7fa901e0a03e717eba2bcc7d3ce23f
bai <1253475445@qq.com>
2016-06-16 20:31:15 +0800

    update order model

--- xiangmu/xiangmu_admin/application/models/Order_model.php
+++ xiangmu/xiangmu_admin/application/models/Order_model.php
@@ -14,19 +14,19 @@ class Order_model extends CI_Model {
         if($border_num==''&&$u_name==''&&$total==''){
             $where=" ";
         }else if($total==''&&$u_name==''){
-            $where="and border.border_number='$border_num'";
+            $where="and border.border_number like '%$border_num%'";
         }else if($total==''&&$border_num==''){
-            $where="and user.user_name='$u_name' ";
+            $where="and user.user_name like '%$u_name%' ";
         }else if($border_num==''&&$u_name==''){
             $where="and border.border_state='$total'";
         }else if($u_name==''){
-            $where="and border.border_number='$border_num' and border.border_state='$total' ";
+            $where="and border.border_number like '%$u_name%' and border.border_state='$total' ";
         }else if($border_num==''){
-            $where="and user.user_name='$u_name' and border.border_state='$total' ";
+            $where="and user.user_name like '%$u_name%' and border.border_state='$total' ";
         }else if($total==''){
-            $where="and user.user_name='$u_name' and border.border_number='$border_num'";
+            $where="and user.user_name like '%$u_name%' and border.border_number like '%$border_num%'";
         }else  if($border_num!=''&&$u_name!=''&&$total!=''){
-            $where="and border.border_state='$total' and user.user_name='$u_name' and border.border_number='$border_num'";
+            $where="and border.border_state='$total' and user.user_name like '%$u_name%' and border.border_number like '%$border_num%'";
         }
         return $this->db->query("select count(*) as c from border inner join user on border.user_id=user.user_id inner join address where address.address_id=border.address_id $where")->result_array();
     }
@@ -35,19 +35,19 @@ class Order_model extends CI_Model {
         if($border_num==''&&$u_name==''&&$total==''){
             $where=" ";
         }else if($total==''&&$u_name==''){
-            $where="and border.border_number='$border_num'";
+            $where="and border.border_number like '%$border_num%'";
         }else if($total==''&&$border_num==''){
-            $where="and user.user_name='$u_name' ";
+            $where="and user.user_name like '%$u_name%' ";
         }else if($border_num==''&&$u_name==''){
             $where="and border.border_state='$total'";
         }else if($u_name==''){
-            $where="and border.border_number='$border_num' and border.border_state='$total' ";
+            $where="and border.border_number like '%$u_name%' and border.border_state='$total' ";
         }else if($border_num==''){
-            $where="and user.user_name='$u_name' and border.border_state='$total' ";
+            $where="and user.user_name like '%$u_name%' and border.border_state='$total' ";
         }else if($total==''){
-            $where="and user.user_name='$u_name' and border.border_number='$border_num'";
+            $where="and user.user_name like '%$u_name%' and border.border_number like '%$border_num%'";
         }else  if($border_num!=''&&$u_name!=''&&$total!=''){
-            $where="and border.border_state='$total' and user.user_name='$u_name' and border.border_number='$border_num'";
+            $where="and border.border_state='$total' and user.user_name like '%$u_name%' and border.border_number like '%$border_num%'";
         }
          return $this->db->query("select * from border inner join user on border.user_id=user.user_id inner join address where address.address_id=border.address_id $where limit $offset,$length")->result_array();
     }   
