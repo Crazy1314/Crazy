0f92a1e8be0ce9b9b4c6ab5058a6fdc8b637b8bc
liying <2104887073@qq.com>
2016-06-17 11:13:34 +0800

    分页方法

--- xiangmu/xiangmu_admin/application/models/Goods_model.php
+++ xiangmu/xiangmu_admin/application/models/Goods_model.php
@@ -16,5 +16,43 @@
 			$query=$this->db->query("SELECT * FROM `goods` inner join goods_type on goods.goods_type_id=goods_type.goods_type_id inner join brand on goods.brand_id=brand.brand_id");
 			return $query->result_array();
 		}
+
+		//所有商品数据
+		function goods_data($search)
+		{
+			$page_size=3;   //设置每页显示条数
+
+			if(is_string($search)){    //判断是否是字符串  如果是就模糊查询
+				$this->db->like('goods_name',$search);
+				$num=$this->db->count_all_results('goods');  
+			}else{
+				$num=$this->db->count_all_results('goods');  //无搜索的总条数
+			}
+			
+			$page_num=ceil($num/$page_size);  //计算总页数
+			//echo $num;die;
+			isset($_GET['page'])?$page=$_GET['page']:$page=1; //当前页
+
+			$limits=($page-1)*$page_size;  //偏移量
+
+			if(is_string($search)){
+				$this->db->like('goods_name',$search);
+			}
+			$query=$this->db
+				->join('goods_type','goods_type.goods_type_id=goods.goods_type_id')
+				->join('brand','brand.brand_id=goods.brand_id')
+				->limit($page_size,$limits)
+				->get('goods');
+			//$query=$this->db->query("SELECT * FROM `goods` inner join goods_type on goods.goods_type_id=goods_type.goods_type_id inner join brand on goods.brand_id=brand.brand_id limit $limits,$page_size");
+			//$query=$this->db->limit($page_size,$limits)->join('goods_type')->where('goods.goods_type_id=goods_type.goods_type_id')->get("goods");
+			$data=$query->result_array();
+			//print_r($data);die;
+			$data['0']['page_num']=$page_num;
+			$data['0']['page']=$page;
+			return $data;
+		}
+
+		//删除
+		//function 
 	}
 ?>
