19bd62be879f5e091dd0818b90dc6a22fadc6465
liying <2104887073@qq.com>
2016-06-17 11:15:44 +0800

    载入公共文件

--- xiangmu/xiangmu_admin/application/views/Goods/insert.php
+++ xiangmu/xiangmu_admin/application/views/Goods/insert.php
@@ -8,50 +8,11 @@
     <script type="text/javascript" src="<?php echo base_url('public')?>/js/libs/modernizr.min.js"></script>
 </head>
 <body>
-<div class="topbar-wrap white">
-    <div class="topbar-inner clearfix">
-        <div class="topbar-logo-wrap clearfix">
-            <h1 class="topbar-logo none"><a href="index.html" class="navbar-brand">商品管理</a></h1>
-            <ul class="navbar-list clearfix">
-                <li><a class="on" href="index.html">首页</a></li>
-                <li><a href="#" target="_blank">网站首页</a></li>
-            </ul>
-        </div>
-        <div class="top-info-wrap">
-            <ul class="top-info-list clearfix">
-                <li><a href="http://www.jscss.me">管理员</a></li>
-                <li><a href="http://www.jscss.me">修改密码</a></li>
-                <li><a href="http://www.jscss.me">退出</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="container clearfix">
-    <div class="sidebar-wrap">
-        <div class="sidebar-title">
-            <h1>菜单</h1>
-        </div>
-        <div class="sidebar-content">
-            <ul class="sidebar-list">
-                <li>
-                    <a href="#"><i class="icon-font">&#xe003;</i>商品管理</a>
-                    <ul class="sub-menu">
-                        <li><a href="<?php echo site_url('Welcome/insert')?>"><i class="icon-font">&#xe008;</i>商品添加</a></li>
-                        <li><a href="<?php echo site_url('Welcome/design')?>"><i class="icon-font">&#xe005;</i>商品列表</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#"><i class="icon-font">&#xe018;</i>系统管理</a>
-                    <ul class="sub-menu">
-                        <li><a href="system.html"><i class="icon-font">&#xe017;</i>系统设置</a></li>
-                        <li><a href="system.html"><i class="icon-font">&#xe037;</i>清理缓存</a></li>
-                        <li><a href="system.html"><i class="icon-font">&#xe046;</i>数据备份</a></li>
-                        <li><a href="system.html"><i class="icon-font">&#xe045;</i>数据还原</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
+
+<!-- 载入公共文件 -->
+<?php $this->load->view('pub.html') ?>
+<!-- 载入公共文件 -->
+
     <!--/sidebar-->
     <div class="main-wrap">
 
