9a9138d1a3f11c7b08ba25508620d9770317803e
bai <1253475445@qq.com>
2016-06-21 19:31:25 +0800

    order model update

--- xiangmu/xiangmu_index/index/Lib/Model/OrderModel.class.php
+++ xiangmu/xiangmu_index/index/Lib/Model/OrderModel.class.php
@@ -20,17 +20,15 @@ class OrderModel extends Model{
 		$goods_id = $data->goods_id;
 		$good = $model->join('goods ON goods_sku.goods_id = goods.goods_id')->where('goods.goods_id ='.$goods_id)->find();
 		$goods=array(
+				'border_goods_id'=>null,
 				'border_id'=>$order_id,
-				'goods_id'=>$good['goods_sku_id'],
+				'goods_sku_id'=>$good['goods_sku_id'],
 				'border_goods_count'=>$data->goods_num,
-				'border_goods_price'=>$good['goods_sku_price'],
 			);
 		$db = M('border_goods');
 		$db->add($goods);	
 		
 	}
-
-
 	/**
 	 * 查询订单
 	 */
@@ -43,9 +41,9 @@ class OrderModel extends Model{
 			$data[$k]['goods'] = $db->join('border ON border.border_id = border_goods.border_id')
 					->join('goods_sku ON border_goods.goods_sku_id = goods_sku.goods_sku_id')
 					->join('goods ON goods_sku.goods_id = goods.goods_id')
+					->order('border.border_id desc')
 					->where('border.border_id = '.$border_id)
 					->select();
-				
 		}
 		
 		return $data;
@@ -58,4 +56,18 @@ class OrderModel extends Model{
 		$data = $model->join('goods ON goods_sku.goods_id = goods.goods_id')->limit(10)->select();
 		return $data;
 	}
+	/**
+	 * 查询订单下商品
+	 */
+	public function selectzne($border_id){
+		$model = M('border_goods');
+		$data = $model->join('goods_sku on goods_sku.goods_sku_id=border_goods.goods_sku_id')
+		->join('goods on goods.goods_id=goods_sku.goods_id')
+		->join('brand on brand.brand_id=goods.brand_id')
+		->join('goods_type on goods_type.goods_type_id=goods.goods_type_id')
+		->where('border_goods.border_id = '.$border_id)
+		->select();
+		return $data;
+	}
 }
+
