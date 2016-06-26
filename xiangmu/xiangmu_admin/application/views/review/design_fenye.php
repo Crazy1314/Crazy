445f9d4e9e17c5a634deb8efa97900890ff2f050
mengyinshuai <1151251718@qq.com>
2016-06-26 21:41:21 +0800

    update

--- xiangmu/xiangmu_admin/application/views/review/design_fenye.php
+++ xiangmu/xiangmu_admin/application/views/review/design_fenye.php
@@ -4,7 +4,7 @@
         <th class="tc" width="5%"><input id="all" class="allChoose" type="checkbox"></th>
         <th>ID</th>
         <th>评论人</th>
-        <th>评论商品id</th>
+        <th>评论商品</th>
         <th>质量等级</th>
         <th>价格等级</th>
         <th>评论内容</th>
@@ -13,26 +13,58 @@
     </tr>
     <?php if(isset($review[0]['review_id'])){
 
-     foreach($review as $k=>$v):?>
-        <tr>
-            <td class="tc"><input class="checkall" type="checkbox"  value="<?php echo $v['review_id']?>"></td>
-            <td><?php echo $v['review_id']?></td>
-            <td><?php echo $v['user_name']?></td>
-            <td><?php echo $v['goods_id']?></td>
-            <td><?php echo $v['quality']?></td>                                
-            <td><?php echo $v['price']?></td>
-            <td><?php echo $v['text']?></td>                               
-            <td><?php echo $v['time']?></td>                               
-            <td>
-                <a class="link-update" href="<?php echo site_url("goods/up?goods_id=").$v['goods_id']?>">修改</a>
-                <a class="link-del" href="<?php echo site_url("goods/add_sku?goods_id=").$v['goods_id']?>">添加属性</a>
-             
-            </td>
-        </tr>
- <?php endforeach;?>
- <?php }else{
-    echo "无数据";die;
-}?>
+        foreach($review as $k=>$v):?>
+            <tr>
+                <td class="tc"><input class="checkall" type="checkbox"  value="<?php echo $v['review_id']?>"></td>
+                <td><?php echo $v['review_id']?></td>
+                <td><?php echo $v['user_name']?></td>
+                <td><?php echo $goods_name[$k][0]['goods_name']?>
+
+                </td>
+                <td>
+                    <?php
+                    if($v['quality']==1){
+                        echo "一星级";
+                    }elseif($v['quality']==2){
+                        echo "二星级";
+                    }elseif($v['quality']==3){
+                        echo "三星级";
+                    }elseif($v['quality']==4){
+                        echo "四星级";
+                    }elseif($v['quality']==5){
+                        echo "五星级";
+                    }elseif($v['quality']==1){
+                        echo "未评价";
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    if($v['price']==1){
+                        echo "一星级";
+                    }elseif($v['price']==2){
+                        echo "二星级";
+                    }elseif($v['price']==3){
+                        echo "三星级";
+                    }elseif($v['price']==4){
+                        echo "四星级";
+                    }elseif($v['price']==5){
+                        echo "五星级";
+                    }elseif($v['price']==1){
+                        echo "未评价";
+                    }
+                    ?>
+                </td>
+                <td><?php echo $v['text']?></td>
+                <td><?php echo $v['time']?></td>
+                <td>
+                    <a class="link-update" href="#" onclick="del_one(<?php echo $v['review_id']?>)" >删除</a>
+                </td>
+            </tr>
+        <?php endforeach;?>
+    <?php }else{
+        echo "无数据";die;
+    }?>
 </table>
     <div class="list-page">
         <a href="javascript:void(0)" value="1" class="last">首页</a>
