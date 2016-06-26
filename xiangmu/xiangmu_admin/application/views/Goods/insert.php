80effadade3824b1d2e5c8ccafff147014da5a1a
mengyinshuai <1151251718@qq.com>
2016-06-26 21:06:16 +0800

    update

--- xiangmu/xiangmu_admin/application/views/Goods/insert.php
+++ xiangmu/xiangmu_admin/application/views/Goods/insert.php
@@ -17,7 +17,7 @@
     <div class="main-wrap">
 
         <div class="crumb-wrap">
-            <div class="crumb-list"><i class="icon-font"></i><a href="/jscss/admin/design/">首页</a><span class="crumb-step">&gt;</span><a class="crumb-name" href="/jscss/admin/design/">作品管理</a><span class="crumb-step">&gt;</span><span>新增作品</span></div>
+            <div class="crumb-list"><i class="icon-font"></i><a href="/jscss/admin/design/">首页</a><span class="crumb-step">&gt;</span><a class="crumb-name" href="/jscss/admin/design/">商品管理</a><span class="crumb-step">&gt;</span><span>新增商品</span></div>
         </div>
         <div class="result-wrap">
             <div class="result-content">
